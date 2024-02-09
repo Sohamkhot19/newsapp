eb1ceb9e25b7f927e831fc5486827352ad774cc7
Sohamkhot19 <sohamkhot2004@gmail.com>
2024-02-09 17:26:29 +0530

    news item updated
    recent news are fetched
    each time news refreshes and new news are displayed
    news container updated

--- src/components/News.js
+++ src/components/News.js
@@ -6,11 +6,11 @@ export class News extends Component {
     return (
       <div>
         <div className="card" style={{width:'22rem'}}>
-          <img src={imgurl} className="card-img-top" alt="..."/>
+          <img src={!imgurl?'https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg':imgurl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 classNmae="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href={newsurl} target='_blank' className="btn btn-sm btn-success">Read More..</a>
+            <p className="card-text">{!description?'Insufficient details available for this news. Please refer to the complete article for a comprehensive understanding.':description}</p>
+            <a rel="noreferrer" href={newsurl} target='_blank' className="btn btn-sm btn-success">Read More..</a>
           </div>
         </div>
       </div>
